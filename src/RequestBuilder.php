264cb81576bc70b023607d2fed0c5d2525bfbe16
andrewtweber <andrew@midwestscc.com>
2020-04-03 13:32:21 -0500

    sort imports alphabetically

--- src/RequestBuilder.php
+++ src/RequestBuilder.php
@@ -2,10 +2,10 @@
 
 namespace Meng\Soap\HttpBinding;
 
-use Psr\Http\Message\RequestInterface;
-use Psr\Http\Message\StreamInterface;
 use Laminas\Diactoros\Request;
 use Laminas\Diactoros\Stream;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\StreamInterface;
 
 /**
  * This class create PSR HTTP requests that embed SOAP messages.
